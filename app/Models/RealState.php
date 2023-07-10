aecae495757e07f0b4177db3abd0cfb1568af48d
souza-wallace <walizdesouza781@gmail.com>
2023-07-09 21:54:11 -0300

    model and controller real state

---
+++ app/Models/RealState.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealState extends Model
+{
+    protected $table = 'real_state';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
